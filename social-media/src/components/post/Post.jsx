9291bfad72b46285582dd81a76162867ee0889cb
shwethaj1104 <shwethaj1104@gmail.com>
2022-09-19 14:28:11 +0530

    Added dynamic posts

--- social-media/src/components/post/Post.jsx
+++ social-media/src/components/post/Post.jsx
@@ -1,7 +1,9 @@
 import { MoreVert } from '@material-ui/icons'
 import './post.css'
 
-const Post = () => {
+const Post = (post) => {
+    console.log("post......",post.post)
+    let inidividualPost = post.post
   return (
     <div className='post'>
         <div className="postWrapper">
@@ -9,24 +11,24 @@ const Post = () => {
                 <div className="postTopLeft">
                     <img src="/assets/person/1.jpeg" alt="" className="postProfileImg" />
                     <span className="postUsername">Shwetha</span>
-                    <span className="postDate">5 mins ago</span>
+                    <span className="postDate">{inidividualPost.date}</span>
                 </div>
                 <div className="postTopRight">
                     <MoreVert />
                 </div>
             </div>
             <div className="postCenter">
-                <span className="postText">Hey! It's my first post :)</span>
-                <img src="/assets/post/1.jpeg" alt="" className="postImg" />
+                <span className="postText">{inidividualPost?.desc} </span>
+                <img src={inidividualPost.photo} alt="" className="postImg" />
             </div>
             <div className="postBottom">
                 <div className="postBottomLeft">
                     <img src="assets/like.png" alt="" className="likeIcon" />
                     <img src="assets/heart.png" alt="" className="likeIcon" />
-                    <span className="postLikeCounter">32 people like it</span>
+                    <span className="postLikeCounter">{inidividualPost.like} people likes it </span>
                 </div>
                 <div className="postBottomRight">
-                    <span className="postCommentText">9 comments</span>
+                    <span className="postCommentText">{inidividualPost.comment} comments </span>
                     </div>
             </div>
         </div>
