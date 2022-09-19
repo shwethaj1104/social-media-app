6b7f5eaf83101a04fdb1634cbbe87aed57138583
shwethaj1104 <shwethaj1104@gmail.com>
2022-09-19 11:44:35 +0530

    Post Changes

--- social-media/src/components/post/Post.jsx
+++ social-media/src/components/post/Post.jsx
@@ -6,7 +6,7 @@ const Post = () => {
     <div className='post'>
         <div className="postWrapper">
             <div className="postTop">
-                <div className="ostTopLeft">
+                <div className="postTopLeft">
                     <img src="/assets/person/1.jpeg" alt="" className="postProfileImg" />
                     <span className="postUsername">Shwetha</span>
                     <span className="postDate">5 mins ago</span>
@@ -15,8 +15,20 @@ const Post = () => {
                     <MoreVert />
                 </div>
             </div>
-            <div className="postCenter"></div>
-            <div className="postBottom"></div>
+            <div className="postCenter">
+                <span className="postText">Hey! It's my first post :)</span>
+                <img src="/assets/post/1.jpeg" alt="" className="postImg" />
+            </div>
+            <div className="postBottom">
+                <div className="postBottomLeft">
+                    <img src="assets/like.png" alt="" className="likeicon" />
+                    <img src="assets/heart.png" alt="" className="likeicon" />
+                    <span className="postLikeCounter">32 people like it</span>
+                </div>
+                <div className="postBottomRight">
+                    <span className="postCommentText">9 comments</span>
+                    </div>
+            </div>
         </div>
     </div>
   )
