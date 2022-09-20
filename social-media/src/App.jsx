16d9d13670d434b08bbf7e0439dc9b5ce4efe9a1
shwethaj1104 <shwethaj1104@gmail.com>
2022-09-20 10:23:45 +0530

    Profile page

--- social-media/src/App.jsx
+++ social-media/src/App.jsx
@@ -1,8 +1,10 @@
 import Home from "./pages/home/Home"
+import Profile from "./pages/profile/Profile"
 
 const App = () => {
   return (
-    <Home />
+    // <Home />
+    <Profile />
   )
 }
 
--- social-media/src/pages/profile/Profile.jsx
+++ social-media/src/pages/profile/Profile.jsx
@@ -1,8 +1,33 @@
 import './profile.css'
+import Feed from "../../components/feed/Feed"
+import Rightbar from "../../components/rightbar/Rightbar"
+import Sidebar from "../../components/sidebar/Sidebar"
+import Topbar from "../../components/topbar/Topbar"
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <>
+    <Topbar />
+    <div className="profile">
+      <Sidebar />
+      <div className="profileRight">
+        <div className="profileRightTop">
+            <div className="profileCover">
+            <img src="/assets/post/3.jpeg" alt="" className="profileCoverImg" />
+            <img src="/assets/post/7.jpeg" alt="" className="profileUserImg" />
+            </div>
+            <div className="profileInfo">
+                <h4 className="profileInfoName">Shwetha</h4>
+                <span className="profileInfoDesc">Hello my friends</span>
+            </div>
+        </div>
+        <div className="profileRightBottom">
+      <Feed />
+      <Rightbar />
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
