4f6e2e1a9ab94df03ac1e72962a07ccbcfcb891c
shwethaj1104 <shwethaj1104@gmail.com>
2022-09-20 11:30:36 +0530

    Implemented routing for home and profile pages

--- social-media/src/components/topbar/Topbar.jsx
+++ social-media/src/components/topbar/Topbar.jsx
@@ -1,7 +1,18 @@
 import { Chat, Notifications, Person, Search } from "@material-ui/icons"
 import "./topbar.css"
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import { useEffect } from "react";
+
 
 const Topbar = () => {
+    const [name,setName]=useState("Go To Profile")
+    const [rounterLink,setRounterLink]=useState("/profile")
+    const changeName=()=>{
+        setName(name==="Go To Profile" ? "Go To Home" : "Go To Profile")
+        setRounterLink(rounterLink==="/profile" ? "/" : "/profile")
+    }
+
     return (
         <div className="topbarContainer">
             <div className="topbarLeft">
@@ -33,6 +44,7 @@ const Topbar = () => {
                     </div>
                 </div>
                 <img src="/assets/person/1.jpeg" alt="profile Image" className="topbarImg" />
+                <Link onClick={changeName} className="goToProfile" to={rounterLink}>{name}</Link>
             </div>
         </div>
     )
