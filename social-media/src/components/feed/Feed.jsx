59831637ab9bbec5d28bf105515843af662c1aa2
shwethaj1104 <shwethaj1104@gmail.com>
2022-09-23 10:25:44 +0530

    code clean up

--- social-media/src/components/feed/Feed.jsx
+++ social-media/src/components/feed/Feed.jsx
@@ -1,16 +1,15 @@
 import Post from '../post/Post'
 import Share from '../share/Share'
 import './feed.css'
-import {Posts} from "../../dummyData"
+import { Posts } from "../../dummyData"
 
 const Feed = () => {
-  console.log("postsssssss...",Posts)
   return (
     <div className='feed'>
       <div className="feedWrapper">
         <Share />
-        {Posts.map(p=>(
-           <Post key={p.id} post={p} />
+        {Posts.map(p => (
+          <Post key={p.id} post={p} />
         ))}
       </div>
     </div>
