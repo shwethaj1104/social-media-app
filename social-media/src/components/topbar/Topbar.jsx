f896a41086e76ab0c2599d95ad63e51474855a38
shwethaj1104 <shwethaj1104@gmail.com>
2022-09-25 10:37:52 +0530

    Code clean up

--- social-media/src/components/topbar/Topbar.jsx
+++ social-media/src/components/topbar/Topbar.jsx
@@ -1,18 +1,9 @@
 import { Chat, Notifications, Person, Search } from "@material-ui/icons"
 import "./topbar.css"
 import { Link } from "react-router-dom";
-import { useState } from "react";
-import { useEffect } from "react";
 
 
 const Topbar = () => {
-    // const [name,setName]=useState("Go To Profile")
-    // const [rounterLink,setRounterLink]=useState("/profile")
-    // const changeName=()=>{
-    //     setName(name==="Go To Profile" ? "Go To Home" : "Go To Profile")
-    //     setRounterLink(rounterLink==="/profile" ? "/" : "/profile")
-    // }
-
     return (
         <div className="topbarContainer">
             <div className="topbarLeft">
