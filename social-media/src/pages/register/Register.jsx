abad1daa5050fe39f55708888a28c537bc77372e
shwethaj1104 <shwethaj1104@gmail.com>
2022-09-26 09:42:50 +0530

    Code clean up

--- social-media/src/pages/register/Register.jsx
+++ social-media/src/pages/register/Register.jsx
@@ -36,7 +36,6 @@ const Register = () => {
                     <span className="loginDesc">Connecting to your friends and family</span>
                 </div>
                 <div className="loginRight">
-                    {/* <div className="loginBox"> */}
                     <form ref={form} className="loginBox">
                         <input placeholder='Username' className="loginInput" value={formDetails.name} onChange={(e) => onFormUpdate('name', e.target.value)} />
                         <input placeholder='Email' className="loginInput" value={formDetails.email} onChange={(e) => onFormUpdate('email', e.target.value)} />
@@ -44,11 +43,8 @@ const Register = () => {
                         <input placeholder='Confirm Password' className="loginInput" value={formDetails.confirmpassword} onChange={(e) => onFormUpdate('confirmpassword', e.target.value)} />
                         <button className="loginButton" onClick={onRegister}>Sign Up</button>
                         {error ? <p className='errorMessage'>{error}</p> : <></>}
-
                         <Link to="/" className="loginRegisterButton">Already have an account?</Link>
-                        {/* <button className="loginRegisterButton" >Login to your account</button> */}
                     </form>
-                    {/* </div> */}
                 </div>
             </div>
         </div>
